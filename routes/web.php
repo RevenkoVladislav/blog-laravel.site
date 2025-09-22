76c95e98fe1d295d3b9fca05b6b7d9d9cae46fab
Vladislav Revenko <Vlad_Reven@mail.ru>
2025-09-22 14:18:38 +0600

    add route and view for admin page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Main;
+use App\Http\Controllers\Admin\Main as AdminMain;
 
 Route::name('main.')->group(function () {
     Route::get('/', Main\IndexController::class)->name('index');
+});
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', AdminMain\IndexController::class)->name('index');
 });
 
 Auth::routes();
