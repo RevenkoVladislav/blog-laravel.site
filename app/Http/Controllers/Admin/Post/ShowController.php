1d36a3d42afdb40ffd6be18de68dcdb110c1bee4
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-03 12:20:50 +0600

    update admin/post/show page

--- app/Http/Controllers/Admin/Post/ShowController.php
+++ app/Http/Controllers/Admin/Post/ShowController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ShowController extends BaseController
 {
     public function __invoke(Post $post)
     {
-        return view('admin.post.show', ['post' => $post]);
+        $tags = $post->tags()->get();
+        return view('admin.post.show', ['post' => $post, 'tags' => $tags]);
     }
 }
--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -4,58 +4,76 @@
     <div class="content-wrapper">
         @include('admin.includes.header')
         <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <!-- Small boxes (Stat box) -->
-                <h1 class="text-center">Пост: {{ $post->title }}</h1>
-                <div class="row d-flex justify-content-center">
-                    <div class="col-8">
-                        <div class="card">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Пост - {{ $post->title }}</h2>
+                    <div class="form-group w-50">
+                        <input type="text" name="title" class="form-control"
+                               value="{{ $post->title }}" disabled>
+                    </div>
 
-                            <div class="card-body table-responsive p-0">
-                                <table class="table table-hover text-nowrap">
-                                    <tbody>
-                                        <tr>
-                                            <td class="text-center">ID</td>
-                                            <td class="text-center">{{ $post->id }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">Название</td>
-                                            <td class="text-center">{{ $post->title }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">Дата создания</td>
-                                            <td class="text-center">{{ $post->created_at->toDateString() }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.card-body -->
+                    <div class="row">
+                        <div class="col-lg-9 mx-auto">
+                            {!! $post->content !!}
                         </div>
-                        <!-- /.card -->
+                    </div>
+
+                    <div class="form-group w-50">
+                        <p>Главное изображение</p>
+                        <img style="width: 30%; height: 30%;" src="{{ asset('/storage/' . $post->main_image)  }}"
+                             alt="нет изображения">
+                    </div>
+
+                    <div class="form-group w-50">
+                        <p>Превью изображение</p>
+                        <img style="width: 30%; height: 30%;" src="{{ asset('/storage/' . $post->preview_image)  }}"
+                             alt="нет изображения">
+                    </div>
+
+                    <div class="form-group w-50">
+                        <label for="">Категория</label>
+                            <p>{{ $post->category->title }}</p>
+                    </div>
+
+                    <div class="form-group w-50">
+                        <label>Тэги</label>
+                        <div class="select2-purple">
+                            <select class="select2" disabled multiple="multiple"
+                                    data-dropdown-css-class="select2-purple" style="width: 100%;">
+                                @foreach($tags as $tag)
+                                    <option selected>{{ $tag->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('tag_ids')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
+            </div>
 
-                <div class="row justify-content-center mb-3">
-                    <div class="col-3">
-                        <a href="{{ route('admin.post.edit', $post) }}" class="btn btn-block btn-success">Редактировать
-                            пост</a>
-                    </div>
+            <div class="row justify-content-center mb-3">
+                <div class="col-3">
+                    <a href="{{ route('admin.post.edit', $post) }}" class="btn btn-block btn-success">Редактировать
+                        пост</a>
                 </div>
+            </div>
 
-                <div class="row justify-content-center mb-3">
-                    <div class="col-3">
-                        <form action="{{ route('admin.post.delete', $post) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-block btn-danger"
-                                    onclick="return confirm('Точно хотите удалить ?')">Удалить пост</button>
-                        </form>
-                    </div>
+            <div class="row justify-content-center mb-3">
+                <div class="col-3">
+                    <form action="{{ route('admin.post.delete', $post) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-block btn-danger"
+                                onclick="return confirm('Точно хотите удалить ?')">Удалить пост
+                        </button>
+                    </form>
                 </div>
+            </div>
 
-            </div><!-- /.container-fluid -->
-        </section>
+        </div><!-- /.container-fluid -->
+
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
