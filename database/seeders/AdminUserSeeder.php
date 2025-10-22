e4a5481bf87cd0226b58107c8208b3b5ec0ea46d
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-22 16:36:25 +0600

    add Admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@mail.ru',
+            'password' => bcrypt('admin'),
+            'role' => User::ROLE_ADMIN,
+            'email_verified_at' => now(),
+        ]);
+    }
+}
