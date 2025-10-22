6e162b526c7488a37663978d7170de2685e81955
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-22 17:13:21 +0600

    add PostFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        Category::factory(15)->create();
+        Category::factory(10)->create();
         Tag::factory(15)->create();
+        Post::factory(30)->create();
         $this->call([AdminUserSeeder::class, SystemCategorySeeder::class]);
     }
 }
